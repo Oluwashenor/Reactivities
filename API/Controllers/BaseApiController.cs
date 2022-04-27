0037d47687bacf6cef599fc5f6daf7eaa1beb5c0
Baba Koodu <adeshiname@gmail.com>
2022-04-27 05:10:30 +0100

    End of Section 2

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        private readonly ILogger<BaseApiController> _logger;
+
+        public BaseApiController()
+        {
+           
+        }
+    }
+}
