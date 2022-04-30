40e1e532888c64245d8e203c4f079f1d82d04566
Baba Koodu <adeshiname@gmail.com>
2022-04-30 12:19:00 +0100

    Modification

--- Application/Activities/Edit.cs
+++ Application/Activities/Edit.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Core;
 using AutoMapper;
 using Domain;
 using MediatR;
@@ -11,11 +12,11 @@ namespace Application.Activities
 {
     public class Edit
     {
-        public class Command:IRequest{
+        public class Command:IRequest<Result<Unit>>{
             public Activity Activity {get;set;}
         }
 
-        public class Handler : IRequestHandler<Command>
+        public class Handler : IRequestHandler<Command, Result<Unit>>
         {
             private readonly DataContext _context;
             private readonly IMapper _mapper;
@@ -25,13 +26,18 @@ namespace Application.Activities
                 _mapper = mapper;
             }
 
-            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            public async Task<Result<Unit>> Handle(Command request, CancellationToken cancellationToken)
             {
                 var activity = await _context.Activities.FindAsync(request.Activity.Id);
-                //activity.Title = request.Activity.Title ?? activity.Title;
+                if(activity==null){
+                    return null;
+                }
                 _mapper.Map(request.Activity, activity);
-                await _context.SaveChangesAsync();
-                return Unit.Value;
+                var result =await _context.SaveChangesAsync() > 0;
+                if(!result){
+                    return Result<Unit>.Failure("Failed To Edit Actiity");
+                }
+                return Result<Unit>.Success(Unit.Value);
             }
         }
     }
